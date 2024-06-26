4ccc00e8b454501ae3fd22f1c8376d31368b5e68
Madesh2003 <mmadeshraina@gmail.com>
2024-06-26 07:24:51 +0530

    setup

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Route, Routes} from 'react-router-dom'
+import {Casting, Movies, MoviesDetails, Navbar, Profile} from './index'
 
 const App = () => (
-  <div>
-    sss
+  <div className='flex h-full'>
+  <Navbar />
+     <main className=' flex-grow p-[2em]'>
+     <div className='h-[70px]'/>
+      <Routes>
+       <Route path='/' element={<Movies />} />
+      </Routes>
+     </main>
   </div>
 );
 
---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Casting } from './Casting/Casting'
+export { default as Movies } from './Movies/Movies'
+export { default as MoviesDetails } from './MovieDetails/Moviedetails'
+export { default as Navbar } from './NavBar/Navbar'
+export { default as Profile } from './Profile/Profile'
+ 
